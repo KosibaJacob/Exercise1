e58595c1ddd60a5036fb3c6e62a083e92e8310e8
KosibaJacob <123763490+KosibaJacob@users.noreply.github.com>
2023-01-27 15:09:32 -0500

    Myinfo

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -79,19 +79,19 @@ public class CardTrick {
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
         
-        System.out.println("My name is Paul, but you can call me prof, Paul or sir");
+        System.out.println("My name is Jacob Kosiba, But you can just call me Jacob!");
         System.out.println();
         
         System.out.println("My career ambitions:");
-        System.out.println("-- Be more active on LinkedIn");
-        System.out.println("-- Have a semester with no violations of academic integrity!");
+        System.out.println("-- To become successful within the tech idustry");
+        System.out.println("-- Become confident with multiple programming languages");
 	System.out.println();	
 
         System.out.println("My hobbies:");
-        System.out.println("-- Investing");
-        System.out.println("-- Cooking");
-        System.out.println("-- Reading/Watching TV");
-        System.out.println("-- Riding my motorcycle");
+        System.out.println("-- Video Games");
+        System.out.println("-- Animals");
+        System.out.println("-- Socializing");
+        System.out.println("-- Watching Netflix");
 
         System.out.println();
         
